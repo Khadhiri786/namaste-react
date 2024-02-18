71d3d937b6b0c1ea9a498709fea985e8117c8167
Khadhiri Abdul Subhan <ams.khadhiri@gmail.com>
2024-02-18 11:05:38 +0530

    Add siblings tag in react

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 /**
 <div id='parent'>
     <div id='child'>
-        <h1></h1>
+        <h1>I'm an h1 tag</h1>
+        <h2>I'm an h2 tag </h2>
     </div>
 </div>
 **/
@@ -9,12 +10,10 @@
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  "Test",
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I'm an h1 tag")
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ])
 );
 
 const heading = React.createElement(
